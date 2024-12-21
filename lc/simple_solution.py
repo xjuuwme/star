296bec334449c4a8dee2ed777edc89ffe908cfed
xjuuwme <xjue@uwm.com>
2024-12-21 14:03:09 -0500

    new

--- lc/simple_solution.py
+++ lc/simple_solution.py
@@ -6,7 +6,20 @@ def contains_duplicate(nums: list[int]):
     return len(set(nums)) != len(nums)
 
 
+def contains_duplicates(nums: list[int]):
+    """" another """
+    seen: set = set()
+    for item in nums:
+        if item in seen:
+            return True
+        seen.add(item)
+    return False
+
+
 if __name__ == "__main__":
+    print(contains_duplicates([1, 2, 3, 4, 5, 1]))
     print(contains_duplicate([1, 2, 3, 4, 5, 1]))
+    print("=====")
+    print(contains_duplicates([1, 2, 3, 4, 5, 7]))
     print(contains_duplicate([1, 2, 3, 4, 5, 7]))
     
