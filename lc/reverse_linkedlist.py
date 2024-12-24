4201130b524c3e456519207f5228ee002ad3ca06
xju <xju@uwm.com>
2024-12-24 14:34:15 -0500

    update bullet point to one line

--- lc/reverse_linkedlist.py
+++ lc/reverse_linkedlist.py
@@ -6,6 +6,8 @@ class ListNode:
 
 
 def print_ll(head):
+    if not head:
+        print("Empty linked list!")
     while head:
         print(f"{head.val} -> ", end="")
         head = head.next
@@ -20,6 +22,14 @@ class Solution:
             current.next = ListNode(i+1, None) 
             current = current.next
         return head
+
+    def build_from(self, node_list: list[int]) -> ListNode:
+        dummy = ListNode(-1, None)
+        pre = dummy
+        for node_val in node_list:
+            pre.next = ListNode(node_val)
+            pre = pre.next
+        return dummy.next 
 
     def reverseList0(self, head: ListNode) -> ListNode:
         pre = None
@@ -40,11 +50,28 @@ class Solution:
             next.next = dummy.next
             dummy.next = next
         return dummy.next
-        
+    
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy = ListNode(-1, None)
+        dummy.next = head
+        pre = dummy
+        while head:
+            if head.val == val:
+                pre.next = head.next
+            else:
+                pre = head
+            head = head.next
+        return dummy.next
+
 
 solution = Solution()
 head = solution.build()
 print_ll(head)
 head2 = solution.reverseList(head)
 print_ll(head2)
+# headb = solution.build_from([6, 6, 1, 2, 6, 3, 4, 6])
+headb = solution.build_from([6, 6])
+# headb = solution.build_from([1, 2])
+print_ll(headb)
+print_ll(solution.removeElements(headb, 6))
             
