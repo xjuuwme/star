8a7d9fdec48871d82acb9e1d5f6eae7570ba20f5
xjuuwme <xjue@uwm.com>
2025-01-11 17:14:11 -0500

    update spiral print

--- lc/power_spiral_matrix.py
+++ lc/power_spiral_matrix.py
@@ -20,13 +20,13 @@ print("==========================")
 ma = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 bb = [*ma.pop(0)]
 cc = [ma.pop()]
-print_ma(bb)
-print_ma(cc)
+print(bb)
+print(cc)
 
 zipped = [list(zip(*ma))[::-1]]
 print_ma(zipped)
 print_ma(zipped[::-1])
 
-print("Result ==========================")
+print("================Recursive Spiral Order of Matrix========================")
 matt = [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
-print_ma(spiralOrder(matt))
+print(spiralOrder(matt))
